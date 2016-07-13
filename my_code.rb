d881e985d2b108d58f40d121973bf55075c6ec6c
Jeff Olson <jolson615@gmail.com>
2016-07-13 12:17:30 +0000

    Done.

---
+++ my_code.rb
@@ -0,0 +1,15 @@
+def shout
+  "WAHOO"
+end
+
+def shout_word(phrase)
+  phrase.upcase + "!!!"
+end
+
+def square(num)
+  num**2
+end
+
+def concatenate_strings(string_1, string_2)
+  "#{string_1} #{string_2}"
+end
