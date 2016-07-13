c8cf870a9fafca79cd14eafa2c10e0a160a64b29
Ela Passarelli <epassarelli18@hb.edu>
2016-07-13 13:53:33 +0000

    Done.

---
+++ my_code.rb
@@ -0,0 +1,15 @@
+def shout
+  "WAHOO"
+end
+
+def shout_word(word="I'm the coolest")
+  word.upcase + "!!!"
+end
+
+def square(number)
+  number*number
+end
+
+def concatenate_strings(string1,string2)
+  string1 + " " + string2
+end
